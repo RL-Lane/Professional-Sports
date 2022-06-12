1f16614064943757eb4dcdb4607db1ec9ddf76a2
Robert Lane <RobertLaneJr@gmail.com>
2022-06-11 23:09:15 -0500

    schema created

---
+++ Resources/schema.sql
@@ -0,0 +1,26 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "teams" (
+    "team" varchar   NOT NULL,
+    "city" varchar(30)   NOT NULL,
+    "league" varchar(3)   NOT NULL,
+    CONSTRAINT "pk_teams" PRIMARY KEY (
+        "team"
+     )
+);
+
+CREATE TABLE "census" (
+    "city" varchar(30)   NOT NULL,
+    "country" varchar(3)   NOT NULL,
+    "2016_population" int   NOT NULL,
+    "2021_population" int   NOT NULL,
+    CONSTRAINT "pk_census" PRIMARY KEY (
+        "city"
+     )
+);
+
+ALTER TABLE "teams" ADD CONSTRAINT "fk_teams_city" FOREIGN KEY("city")
+REFERENCES "census" ("city");
+
